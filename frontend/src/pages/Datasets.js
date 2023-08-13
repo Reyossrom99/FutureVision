863c4fe9c0ef3e5c9826bf0565b5deecfa6ff28d
REYESPC <reyesossorio@gmail.com>
2023-08-13 13:36:40 +0200

    primera view de las cartas de los datasets + sidenav

--- frontend/src/pages/Datasets.js
+++ frontend/src/pages/Datasets.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react'; 
 import axios from 'axios'; 
 import FormDialog from '../components/newDatasetForm';
+import './datasets.css'
+import SideNav from '../components/SideNav'
 
 
 //make http request to /datasets when enter the page 
@@ -38,26 +40,35 @@ function Datasets(){
     }
 
     return (
-        <div>
+      <div className='page-container'>
+        <div className="sidenav">
+        <SideNav /> {/* Use the SideNav component */}
+      </div>
+        <div className='content-container'>
         <h1>Dataset Information</h1>
-        {datasets.length > 0 ? (
-          datasets.map(dataset => (
-            <div key={dataset.id}>
-              <h2>{dataset.name}</h2>
-              <p>Description: {dataset.description}</p>
-              <p>Uploaded Date: {dataset.uploaded_date}</p>
-              <p>URL: <a href={dataset.url} target="_blank" rel="noopener noreferrer">{dataset.url}</a></p>
-              <hr />
-            </div>
-          ))
-        ) : (
-          //there are no datasets avariable so we put a botton to create a new one
-          <p>No datasets avariable</p>
-          
-        )}
+        <div className="datasets-container">
+          {datasets.length > 0 ? (
+            datasets.map(dataset => (
+              <div key={dataset.id} className="dataset-card">
+                <img
+                  src={"C:7Users/reyes/Pictures/fondo_naranja.jpg"} // Replace with the actual image URL from your dataset
+                  alt={dataset.name}
+                  className="dataset-image"
+                />
+                <div className="dataset-info">
+                  <h2 className="dataset-name">{dataset.name}</h2>
+                  <p className="dataset-description">{dataset.description}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No datasets available</p>
+          )}
+        </div>
         <button onClick={handlePostRequest}>SEND POST</button>
         <button onClick={() => setIsDialogOpen(true)}>Open Form Dialog</button>
         <FormDialog isOpen={isDialogOpen} onRequestClose={() => setIsDialogOpen(false)} />
+      </div>
       </div>
     ); 
 }
