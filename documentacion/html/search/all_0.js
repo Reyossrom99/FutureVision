0d9c69962cba217da100f44364d8ae0f42416ea7
REYESPC <reyesossorio@gmail.com>
2023-09-05 09:22:49 +0200

    gitignore apply changes

--- documentacion/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['datasetsconfig_0',['DatasetsConfig',['../classdatasets_1_1apps_1_1_datasets_config.html',1,'datasets::apps']]]
-];
--- documentacion/html/search/namespaces_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['asgi_0',['asgi',['../namespacesrc_1_1asgi.html',1,'src']]],
-  ['settings_1',['settings',['../namespacesrc_1_1settings.html',1,'src']]],
-  ['urls_2',['urls',['../namespacesrc_1_1urls.html',1,'src']]],
-  ['wsgi_3',['wsgi',['../namespacesrc_1_1wsgi.html',1,'src']]]
-];
